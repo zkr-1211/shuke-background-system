a0cca58c178c7ada42e560fe8e815012e06f7985
zhangkr <zhangkr>
2023-03-15 15:03:21 +0800

    upd

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,6 @@
 const path = require("path");
 const resolve = dir => path.join(__dirname, dir);
 module.exports = {
-  assetsDir: 'static',
-  parallel: false,
-  publicPath: './',
   // lintOnSave: false,
   chainWebpack: config => {
     config.resolve.alias
